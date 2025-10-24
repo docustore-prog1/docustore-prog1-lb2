a8103a6bdd2b6f123b6e5e01e217dd2cf2d878e5
Philippe Reichen <pre120662@stud.gibb.ch>
2025-10-24 11:07:26 +0200

    Setup Project

---
+++ Archivsoftware/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Archivsoftware
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
