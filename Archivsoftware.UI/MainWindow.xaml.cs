3e4f48067d727cb5f576a81f6d3419325515a1da
Lukas Küffer <lku120333@stud.gibb.ch>
2025-11-14 11:19:39 +0100

    Added file picker to UI

--- Archivsoftware.UI/MainWindow.xaml.cs
+++ Archivsoftware.UI/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace Archivsoftware
 {
@@ -19,6 +11,20 @@ namespace Archivsoftware
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenFilePicker(object sender, RoutedEventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog
+            {
+                Title = "Select Files and Folders",
+                IsFolderPicker = true,
+                Multiselect = true,
+                InitialDirectory = Environment.GetFolderPath(
+                    Environment.SpecialFolder.MyDocuments
+                )
+            };
+            dialog.ShowDialog();
         }
     }
 }
